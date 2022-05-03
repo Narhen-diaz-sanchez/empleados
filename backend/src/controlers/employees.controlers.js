2ca9dca4907f67dcdd83d0ddff7ed52321ce349e
Narhen Diaz Sanchez <narhen2015@gmail.com>
2022-05-03 18:05:42 -0500

    Segundo commit, backend funcionando

--- backend/src/controlers/employees.controlers.js
+++ backend/src/controlers/employees.controlers.js
@@ -1,17 +1,51 @@
 const employeeCont = {}
 const Employee = require("../models/employee");
 
-employeeCont.getEmployees = async (req, res) => {
-    const employees = await Employee.find()
-    res.json(employees)
-}
-employeeCont.createEmployee = (req, res) => {
-    res.send('create employees')
-}
-employeeCont.getEmployee = (req, res) => {
-    res.send('get employee')
-}
-employeeCont.editEmployee = (req, res) => {}
-employeeCont.deleteEmployee = (req, res) => {}
+employeeCont.getEmployees = async (req, res, next) => {
+    const employees = await Employee.find();
+    res.json(employees);
+};
+
+employeeCont.createEmployee = async (req, res, next) => {
+    const employee = new Employee({
+        name: req.body.name,
+        position: req.body.position,
+        office: req.body.office,
+        salary: req.body.salary,
+    });
+    await employee.save();
+    res.json({
+        status: "Employee created"
+    });
+};
+
+employeeCont.getEmployee = async (req, res, next) => {
+    const {
+        id
+    } = req.params;
+    const employee = await Employee.findById(id);
+    res.json(employee);
+};
+
+employeeCont.editEmployee = async (req, res, next) => {
+    const {
+        id
+    } = req.params;
+    await Employee.findByIdAndUpdate(id, {
+        $set: req.body
+    }, {
+        new: true
+    });
+    res.json({
+        status: "Employee Updated"
+    });
+};
+
+employeeCont.deleteEmployee = async (req, res, next) => {
+    await Employee.findByIdAndRemove(req.params.id);
+    res.json({
+        status: "Employee Deleted"
+    });
+};
 
 module.exports = employeeCont;
